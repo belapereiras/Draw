6f45bb0cb59ef36826cbe626890e3f7f1c504d8a
belapereiras <isabela_94@msn.com>
2016-11-11 11:47:12 -0200

    Initial commit

---
+++ Doodle/Doodle/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  Doodle
+//
+//  Created by Isabela Pereira on 11/10/16.
+//  Copyright © 2016 Isabela Pereira. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    //SET UP LEVEL SCENE
+
+    let sceneNode = LevelScene(size: view.frame.size)
+    
+    if let view = self.view as! SKView? {
+        view.presentScene(sceneNode)
+        view.ignoresSiblingOrder = true
+        
+        view.showsFPS = false
+        view.showsNodeCount = false
+    }
+}
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
---
+++ Doodle/LevelScene.swift
@@ -0,0 +1,79 @@
+//
+//  MenuScene.swift
+//  Doodle
+//
+//  Created by Isabela Pereira on 11/10/16.
+//  Copyright © 2016 Isabela Pereira. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import SpriteKit
+import CoreGraphics
+
+class LevelScene: SKScene {
+    
+    private var lastUpdateTime : TimeInterval = 0
+    
+    override func sceneDidLoad() {
+        self.lastUpdateTime = 0
+        
+    //SET UP BACKGROUND
+        
+        let background = SKSpriteNode(imageNamed: "BG")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.zPosition = 0
+        
+        addChild(background)
+        
+    // SET UP FLOOR
+        
+        let floor = SKSpriteNode(imageNamed: "floor")
+        floor.position = CGPoint(x: size.width / 2, y: 110)
+        floor.zPosition = 1
+        
+        floor.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: -size.width / 2, y: 0), to: CGPoint(x: size.width, y: 0)) //ajustar a "moldura" do chao, pra quando a bola cair nao ultrapassar
+        floor.physicsBody?.isDynamic = false
+        
+        addChild(floor)
+        
+    //SET UP BALL
+
+        let ball = SKSpriteNode(imageNamed: "ball")
+        ball.position = CGPoint(x: 390, y: 500) //y: 160 se eu quiser que a bola caia
+        ball.zPosition = 1
+        
+        ball.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 20, height: 20))
+        
+        addChild(ball)
+        
+    //SET UP HOLE
+        
+        let hole = SKSpriteNode(imageNamed: "hole")
+        hole.position = CGPoint(x: 1670, y: 230)
+        hole.zPosition = 1
+        
+        addChild(hole)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        self.lastUpdateTime = currentTime
+    }
+    
+    
+    
+}
